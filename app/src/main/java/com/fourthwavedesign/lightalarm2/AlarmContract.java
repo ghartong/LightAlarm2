b16989b8cf0fa42feaea0898c3e8e55120b934b8
ghartong <glenn@glickcf.com>
2014-12-15 23:44:41 -0500

    rewrite

---
+++ app/src/main/java/com/fourthwavedesign/lightalarm2/AlarmContract.java
@@ -0,0 +1,23 @@
+package com.fourthwavedesign.lightalarm2;
+
+/**
+ * Created by reddog on 12/14/14.
+ */
+import android.provider.BaseColumns;
+
+public final class AlarmContract {
+
+    public AlarmContract() {}
+
+    public static abstract class Alarm implements BaseColumns {
+        public static final String TABLE_NAME = "alarm";
+        public static final String COLUMN_NAME_ALARM_NAME = "name";
+        public static final String COLUMN_NAME_ALARM_TIME_HOUR = "hour";
+        public static final String COLUMN_NAME_ALARM_TIME_MINUTE = "minute";
+        public static final String COLUMN_NAME_ALARM_REPEAT_DAYS = "days";
+        public static final String COLUMN_NAME_ALARM_REPEAT_WEEKLY = "weekly";
+        public static final String COLUMN_NAME_ALARM_TONE = "tone";
+        public static final String COLUMN_NAME_ALARM_ENABLED = "enabled";
+    }
+
+}
---
+++ app/src/main/java/com/fourthwavedesign/lightalarm2/AlarmService.java
@@ -0,0 +1,32 @@
+package com.fourthwavedesign.lightalarm2;
+
+/**
+ * Created by reddog on 12/15/14.
+ */
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class AlarmService extends Service {
+
+    public static String TAG = AlarmService.class.getSimpleName();
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Intent alarmIntent = new Intent(getBaseContext(), AlarmScreen.class);
+        alarmIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        alarmIntent.putExtras(intent);
+        getApplication().startActivity(alarmIntent);
+
+        AlarmManagerHelper.setAlarms(this);
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+}
