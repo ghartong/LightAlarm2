0b5e8807990f19f4cb9c899249057cef8a25f4ae
ghartong <glenn@glickcf.com>
2014-12-21 12:40:41 -0500

    added debug on/off, notifications on/off

--- app/src/main/java/com/fourthwavedesign/lightalarm2/RequestTask.java
+++ app/src/main/java/com/fourthwavedesign/lightalarm2/RequestTask.java
@@ -1,7 +1,14 @@
 package com.fourthwavedesign.lightalarm2;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -22,6 +29,10 @@ import java.io.IOException;
 class RequestTask extends AsyncTask<String, String, String> {
     public Context mContext;
     public int mDuration = Toast.LENGTH_LONG;
+
+    private NotificationManager mNotificationManager;
+    private int notificationID = 100;
+    private int numMessages = 0;
 
     public RequestTask(Context context){
         mContext=context;
@@ -60,10 +71,52 @@ class RequestTask extends AsyncTask<String, String, String> {
     protected void onPostExecute(String result) {
         super.onPostExecute(result);
         //Do anything with response..
+
+        //get api vars from prefs or default to "FALSE"
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(mContext);
+        Boolean apidebug = sharedPrefs.getBoolean("apidebug", false);
+        Boolean notify = sharedPrefs.getBoolean("notify", false);
+
         if(!result.equals("failed")) {
-            //Log.v("RequestTask", result);
-            Toast toast = Toast.makeText(mContext, result, mDuration);
-            toast.show();
+            Log.v("RequestTask", "Debug: " + apidebug);
+            if(apidebug == true) {
+                //Log.v("RequestTask", result);
+                Toast toast = Toast.makeText(mContext, result, mDuration);
+                toast.show();
+            }
+        }
+
+        if(notify == true) {
+            //Log.i("Request Task", "notification started");
+          /* Invoking the default notification service */
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(mContext);
+
+            mBuilder.setContentTitle(mContext.getString(R.string.notification_title));
+            mBuilder.setContentText(mContext.getString(R.string.notification_text));
+            mBuilder.setTicker(mContext.getString(R.string.notification_ticker));
+            mBuilder.setSmallIcon(R.drawable.ic_launcher);
+            mBuilder.setAutoCancel(true);
+
+          /* Increase notification number every time a new notification arrives */
+            mBuilder.setNumber(++numMessages);
+
+          /* Creates an explicit intent for an Activity in your app */
+            Intent resultIntent = new Intent(mContext, AlarmListActivity.class);
+            PendingIntent resultPendingIntent =
+                    PendingIntent.getActivity(
+                            mContext,
+                            0,
+                            resultIntent,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+            mBuilder.setContentIntent(resultPendingIntent);
+
+            mNotificationManager =
+                    (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+
+          /* notificationID allows you to update the notification later on. */
+            mNotificationManager.notify(notificationID, mBuilder.build());
         }
     }
 }
